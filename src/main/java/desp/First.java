0c63738cb0fa45d217f231a376785a3f8eeb0c86
desperatio_Q <1594552832@qq.com>
2020-02-20 21:43:32 +0800

    1

---
+++ src/main/java/desp/First.java
@@ -0,0 +1,13 @@
+package desp;
+
+/**
+ * 1
+ *
+ * @authoor Desperatio_QYL
+ * @create 2020-02-20-10:52
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
